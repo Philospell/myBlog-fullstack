020373a194d4b1687655375092ff95592c11ccd6
BYUNGMIN CHOI <nekarakube@gmail.com>
2025-06-15 22:08:26 +0900

    get posts router 작성. db.execute 일 떈 sql 로 변수 수정

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,32 @@ const router = express.Router();
 const db = require('../db');
 const { isLoggedIn } = require('../middlewares/isLoggedIn');
 
+/**
+ * GET
+ */
+
+router.get('/', async (req, res) => {
+    try {
+        const query = `
+        SELECT posts.id, posts.title, posts.created_at, users.nickname
+        FROM posts
+        JOIN users ON posts.user_id = users.id
+        ORDER BY posts.created_at DESC
+        `;
+
+        const [rows] = await db.promise().query(query);
+
+        res.status(200).json(rows);
+    } catch (error) {
+        console.error('게시글 목록 조회 실패:', error);
+        res.status(500).json({ message: '서버 오류' });
+    }
+})
+
+/**
+ * POST
+ */
+
 router.post('/', isLoggedIn, async (req, res) => {
     try {
         const { title, content } = req.body;
@@ -12,8 +38,8 @@ router.post('/', isLoggedIn, async (req, res) => {
             return res.status(400).json({ message: '제목과 내용을 입력해주세요.' });
         }
 
-        const query = 'INSERT INTO posts (title, content, user_id) VALUES (?, ?, ?)';
-        const result = await db.execute(query, [title, content, userId]);
+        const sql = 'INSERT INTO posts (title, content, user_id) VALUES (?, ?, ?)';
+        const result = await db.execute(sql, [title, content, userId]);
 
         res.status(201).json({ message: '글이 작성되었습니다.', postId: result.insertId });
     } catch (error) {
