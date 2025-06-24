583af63ce1b776d8da5c0448a26c8bb28d0a5ce7
BYUNGMIN CHOI <nekarakube@gmail.com>
2025-06-24 22:09:07 +0900

    /login, /new Route 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import PostList from './components/PostList';
 import PostDetail from './components/PostDetail';
+import NewPost from './components/NewPost';
+import Login from './components/Login';
 
 export default function App() {
     return (
         <Routes>
-            <Route path='/' element={<PostList />}></Route>
-            <Route path='/posts/:id' element={<PostDetail />}></Route>
+            <Route path='/' element={<PostList />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/posts/:id' element={<PostDetail />} />
+            <Route path='/new' element={<NewPost />} />
         </Routes>
     )
 }
